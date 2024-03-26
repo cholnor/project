81f0092e3e8d9658b6223d376bb9275cf4b43251
cholnor <skumar11137@gmail.com>
2024-03-26 14:10:31 +0530

    filder updated

--- template.py
+++ template.py
@@ -1,30 +1,35 @@
-
 import os
 from pathlib import Path
 list_of_files=[
     "githu/workflow/.gitkeep",
+    "artifacts/__init__.py",
+    "experiment/expiriment.ipynb",
+    "mlrun/__init_.py",
     "src/__init__.py",
+    "src/component/__init__.py",
     "src/component/data_ingestion.py",
     "src/component/data_transformation.py",
     "src/component/model_trainer.py",
     "src/component/model_evaluation.py",
+    "src/exception/__init__.py",
+    "src/exception/exception.py",
+    "src/exception/exception.pytests/unit/__init__",
+    "src/logger/logging.py",
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
     "src/utils/__init__.py",
     "src/utils/utils.py",
-    "src/logger/logging.py",
-    "src/exception/exxception.py",
     "src/tests/unit/__init__.py",
     "src/tests/integration/__init__.py",
+    "app.py",
     "init_setup.sh",
+    "pyprojects.toml",
     "requirement.text",
+    "requirements_dev.txt",
     "setup.py",
     "setup.cnf",
-    "pyprojects.toml",
     "tox.ini",
-    "experiment/experiment.ipynb",
-
 ]
 
 for filepath in list_of_files:
